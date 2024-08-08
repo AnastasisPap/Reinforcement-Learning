8a82c739a551f76fc7a7a63d58043e4e399939a1
Anastasis Papapanagiotou <anastasis.papapanagiotou@gmail.com>
2024-08-08 12:40:44 +0300

    Added more initialization types for value functions of the base agent.

--- libs/utils/agent.py
+++ libs/utils/agent.py
@@ -12,9 +12,15 @@ class BaseAgent:
         self.alpha = args.get('alpha', 0.1)
         self.env = env
 
+        init_value = args.get('init_value', 0.0)
         dims = (env.env_dimensions,) if type(env.env_dimensions) is int else env.env_dimensions
-        self.Q = np.zeros((*dims, env.action_space.n))
-        self.V = np.zeros(dims)
+
+        if init_value == 'random':
+            self.Q = np.random.uniform(size=(*dims, env.action_space.n))
+            self.V = np.random.uniform(size=dims)
+        else:
+            self.Q = np.full((*dims, env.action_space.n), init_value)
+            self.V = np.full(dims, init_value)
     
     def eps_greedy(self, s):
         """It's the epsilon greedy policy. With probability epsilon selects any action
